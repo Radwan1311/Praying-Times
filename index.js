4129bb80034d33014aedde88a280ab482118ac67
Radwan1311 <radwan.1311.mr@gmail.com>
2022-09-09 00:13:38 +0200

    changing var to let

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ $("#find_btn").click(function () { //user clicks button
 	if ("geolocation" in navigator){ //check geolocation available
 		    //try to get user current location using getCurrentPosition() method
 		    navigator.geolocation.getCurrentPosition(function(position){
-        var long = position.coords.longitude ;
-        var lati = position.coords.latitude ;
+        let long = position.coords.longitude ;
+        let lati = position.coords.latitude ;
 
         // converting the user location cord (latitude , longitude) to the city name and the country
         $.get( "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lati + "," +long+"&key=AIzaSyBgO1PpmaQgelvqbrCkIiDg5eAmyVV6sPA&sensor=false", function(data) {
-        var city = data.results[1].address_components[3].long_name;
-        var country = data.results[1].address_components[4].long_name;
-        
+        let city = data.results[1].address_components[3].long_name;
+        let country = data.results[1].address_components[4].long_name;
+
 
 
           // lets apply the user location to get the correct praying times
@@ -19,11 +19,11 @@ $("#find_btn").click(function () { //user clicks button
 
 
           // now we set the praying times accordings to the user location
-          var fajr =   $('#fajr').html('Fajr time is : ' +data2.data[0].timings.Fajr );
-          var zohr =   $('#zohr').html('zohr time is : ' + data2.data[0].timings.Dhuhr);
-          var asr =  $('#asr').html('asr time is : ' + data2.data[0].timings.Asr);
-          var magrib =   $('#magrib').html('magrib time is : ' + data2.data[0].timings.Maghrib);
-          var ishaa =   $('#ishaa').html('ishaa time is : ' + data2.data[0].timings.Isha);
+          let fajr =   $('#fajr').html('Fajr time is : ' +data2.data[0].timings.Fajr );
+          let zohr =   $('#zohr').html('zohr time is : ' + data2.data[0].timings.Dhuhr);
+          let asr =  $('#asr').html('asr time is : ' + data2.data[0].timings.Asr);
+          let magrib =   $('#magrib').html('magrib time is : ' + data2.data[0].timings.Maghrib);
+          let ishaa =   $('#ishaa').html('ishaa time is : ' + data2.data[0].timings.Isha);
 
         });
       });
